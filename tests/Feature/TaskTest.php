7c7856da8ba64173aa3e13a64d8a96a53af6fe16
Shovon <zahidul.cse14@gmail.com>
2024-01-26 15:24:37 +0600

    todo list add, update, remove testing

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use App\Models\Todo;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class TaskTest extends TestCase
 {
@@ -22,6 +24,42 @@ class TaskTest extends TestCase
         $response->assertStatus(200);
     }
 
+    public function test_if_todo_working(): void
+    {
+        $response = Todo::create([
+            'task' => 'task 5.1',
+            'status' => 'pending',
+            'figure' => 'dfukcingmeasery.jpg'
+        ]);
+
+        $response->update([
+            'task' => 'task 5.1.1',
+            'status' => 'Updated',
+            'figure' => 'addedoldphotoasnew.jpg',
+        ]);
+
+        $response->delete();
+
+        $response = $this->get('/tasks');
+
+        $response->assertSee('404');
+    }
+
+    // public function test_if_file_upload_working(): void
+    // {
+    //     Storage::fake('avatars');
+
+    //     $file = UploadedFile::fake()->image('avatar.jpg');
+
+    //     $response = $this->post('/avatar', [
+    //         'avatar' => $file,
+    //     ]);
+
+    //     Storage::disk('avatars')->assertExists($file->hashName());
+    // }
+
+
+
     public function check_has_tasks(): void
     {
         $tasks = Todo::factory()->create();
@@ -34,18 +72,5 @@ class TaskTest extends TestCase
         $tasks->assertSee('404');
     }
 
-    public function test_create_task(): void{
-        $tasks = Todo::create([
-            'task' => '$request->task',
-            'status' => '$request->status',
-            'figure' => 'dsklfldsaife.jpg'
-        ]);
-        // $tasks->assertOk();
-        // error_log($tasks);
 
-
-        $tasks = $this->get('/tasks');
-        $tasks->assertSeeText(404);
-        // $response->assertDontSee("No task founded");
-    }
 }
